d5b59053e49ffb4683105ca73b4eac5d129c3501
fernando-rodrigues-dev <fvetor@gmail.com>
2023-11-22 11:14:42 -0300

    Create catalog_id.ml.js

---
+++ catalog_id.ml.js
@@ -0,0 +1,24 @@
+function run(){
+    var style = document.createElement('style')
+    style.innerText = `.mlid-cat{
+background: red;
+position: absolute;
+z-index: 999;
+padding: 1px 8px;
+color: white;
+font-weight: bold;}`
+    document.body.appendChild(style)
+
+    var href
+    var mlid
+    $('.ui-search-layout__item').each(function(i){
+        href = $(this).find('a').attr('href')
+        if(href.substr(0,11) == 'https://www'){
+            mlid = href.split('/')[5].split('?')[0]
+            $(this).prepend('<div class="mlid-cat">'+mlid+'</div>')
+        }
+    })
+}
+
+run();
+setTimeout(run, 5000);
